477d06734ecb5c15bd3024dd979fc4f85dec1bbf
Stephen Cleary <github.ourteddybear@xoxy.net>
2022-02-12 11:37:56 -0500

    Add allocator of stream identifiers.

--- Nito.Quicly/Nito.Quicly/StreamIdentifier.cs
+++ Nito.Quicly/Nito.Quicly/StreamIdentifier.cs
@@ -1,7 +1,7 @@
 ﻿namespace Nito.Quicly
 {
     /// <summary>
-    /// Represents a stream identifier. This is a 62-bit unsigned integer.
+    /// Represents a stream identifier. This is a 62-bit unsigned integer, which is comprised of a 60-bit unsigned counter and two bits of stream attributes.
     /// </summary>
     public readonly record struct StreamIdentifier
     {
@@ -10,7 +10,7 @@
         /// </summary>
         public StreamIdentifier(ulong value)
         {
-            if (value >= ExclusiveUpperBound)
+            if (value >= (CounterExclusiveUpperBound << 2))
                 throw new ArgumentOutOfRangeException(nameof(value));
 
             Value = value;
@@ -21,7 +21,7 @@
         /// </summary>
         public StreamIdentifier(ulong counter, bool isServerInitiated, bool isUnidirectional)
         {
-            if (counter >= (ExclusiveUpperBound >> 2))
+            if (counter >= CounterExclusiveUpperBound)
                 throw new ArgumentOutOfRangeException(nameof(counter));
 
             Value = (counter << 2) | (isServerInitiated ? 0x1 : 0x0ul) | (isUnidirectional ? 0x2 : 0x0ul);
@@ -31,6 +31,11 @@
         /// The underlying value.
         /// </summary>
         public ulong Value { get; }
+
+        /// <summary>
+        /// The integer counter of the stream identifier within its stream space.
+        /// </summary>
+        public ulong Counter => Value >> 2;
 
         /// <summary>
         /// Whether this stream is server-initiated. If <c>false</c>, then this stream is client-initiated.
@@ -53,8 +58,8 @@
         public bool IsBidirectional => !IsUnidirectional;
 
         /// <summary>
-        /// 2**62
+        /// 2**60
         /// </summary>
-        private const ulong ExclusiveUpperBound = 4611686018427387904;
+        private const ulong CounterExclusiveUpperBound = 1152921504606846976;
     }
 }
---
+++ Nito.Quicly/Nito.Quicly/StreamIdentifierManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nito.Quicly
+{
+    /// <summary>
+    /// A manager that handles creating stream identifiers for a specific stream space.
+    /// </summary>
+    public sealed class StreamIdentifierManager
+    {
+        /// <summary>
+        /// Creates a manager for allocating stream identifiers within a specific stream space.
+        /// </summary>
+        /// <param name="isServerInitiated">Whether stream identifiers created by this manager are server-initiated.</param>
+        /// <param name="isUnidirectional">Whether stream identifiers created by this manager are unidirectional.</param>
+        public StreamIdentifierManager(bool isServerInitiated, bool isUnidirectional)
+        {
+            IsServerInitiated = isServerInitiated;
+            IsUnidirectional = isUnidirectional;
+        }
+
+        /// <summary>
+        /// Whether stream identifiers created by this manager are server-initiated.
+        /// </summary>
+        public bool IsServerInitiated { get; }
+
+        /// <summary>
+        /// Whether stream identifiers created by this manager are client-initiated.
+        /// </summary>
+        public bool IsClientInitiated => !IsServerInitiated;
+
+        /// <summary>
+        /// Whether stream identifiers created by this manager are unidirectional.
+        /// </summary>
+        public bool IsUnidirectional { get; }
+
+        /// <summary>
+        /// Whether stream identifiers created by this manager are bidirectional.
+        /// </summary>
+        public bool IsBidirectional => !IsUnidirectional;
+
+        /// <summary>
+        /// Creates the next stream identifier in this space. This method is thread-safe.
+        /// </summary>
+        public StreamIdentifier CreateStreamIdentifier()
+        {
+            var counter = Interlocked.Increment(ref _counter);
+            return new StreamIdentifier(counter, IsServerInitiated, IsUnidirectional);
+        }
+
+        private ulong _counter = ulong.MaxValue;
+    }
+}
